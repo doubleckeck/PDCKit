59a34e504b796df4b8953096e0a7d9bd71b6f076
PDC <doublecheck@yeah.net>
2016-07-01 17:18:01 +0800

    修改描述错误

--- PDCKit/PDCKit/PDCAdd/NSKit/NSArray+PDCAdd.h
+++ PDCKit/PDCKit/PDCAdd/NSKit/NSArray+PDCAdd.h
@@ -12,14 +12,14 @@ typedef void(^block_array)(NSInteger index,id obj);
 
 @interface NSArray (PDCAdd)
 /**
- *  异步队列遍历数组，遍历结果是无序的
+ *  异步并行队列遍历数组，遍历结果是无序的
  *
  *  @param block callback
  */
 -(void )asynParallelEnumerateObjectsUsingBlock:(block_array )block;
 
 /**
- *  同步队列遍历数组，遍历结果是有序的
+ *  异步串行队列遍历数组，遍历结果是有序的
  *
  *  @param block callback
  */
