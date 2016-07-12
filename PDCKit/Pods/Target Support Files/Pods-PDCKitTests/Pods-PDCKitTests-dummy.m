c3813151acb08c39ceb22372ab09aea9459194ba
PDC <doublecheck@yeah.net>
2016-07-12 11:38:02 +0800

    remove pods

--- PDCKit/Pods/Target Support Files/Pods-PDCKitTests/Pods-PDCKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PDCKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_PDCKitTests
-@end
--- PDCKit/Pods/Target Support Files/Pods-PDCKitUITests/Pods-PDCKitUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PDCKitUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PDCKitUITests
-@end
